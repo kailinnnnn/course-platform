66895ba80e8df48ad80ef9d6399214c20d953c76
kailin yang <kailinnnnn0408@gmail.com>
2023-06-01 20:13:11 +0800

    update findone

--- server/config/passport.js
+++ server/config/passport.js
@@ -7,17 +7,17 @@ module.exports = (passport) => {
   opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme("jwt");
   opts.secretOrKey = process.env.PASSPORT_SECRET;
   passport.use(
-    new JwtStrategy(opts, function (jwt_payload, done) {
-      User.findOne({ _id: jwt_payload._id }, (err, user) => {
-        if (err) {
-          return done(err, false);
-        }
+    new JwtStrategy(opts, async function (jwt_payload, done) {
+      try {
+        const user = await User.findOne({ _id: jwt_payload._id });
         if (user) {
-          done(null, user);
+          return done(null, user);
         } else {
-          done(null, false);
+          return done(null, false);
         }
-      });
+      } catch (err) {
+        return done(err, false);
+      }
     })
   );
 };
